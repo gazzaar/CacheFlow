994aa486199a5de789dc2eab44ac55c3dcecfa9c
gazzaar <gazzarDev@gmail.com>
2025-08-08 17:44:46 +0300

    feat: Implement CLI

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import { Command } from 'commander';
+import { startServer } from './index';
+import { validateArguments } from './util/validateArgument';
+import { clearCache } from './cacheManager';
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('A CLI Caching Proxy server')
+  .option('-p, --port  <value>', 'Specify Port number')
+  .option('-o, --origin <value>', 'Specify Origin server')
+  .option('-c, --clear-cache ', 'Clear cache')
+  .parse(process.argv);
+const options = program.opts();
+
+if (options.clearCache) {
+  clearCache();
+  console.log('clearning');
+  process.exit();
+} else {
+  validateArguments(options.port, options.origin, startServer, program);
+}
